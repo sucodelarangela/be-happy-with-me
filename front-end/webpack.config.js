5f525daea3e46c36f4f497aeff232dfe1b334f0a
Angela Caldas <aes.caldas@gmail.com>
2022-05-21 19:53:31 -0300

    Installing Pure.css, style-loader and css-loader for dealing with styles

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack') // node_modules
-const path = require('path') // core do Node.js
-const HtmlWebpackPlugin = require('html-webpack-plugin') // gera o html em dist automaticamente com base em um modelo na pasta src
+const webpack = require('webpack'); // node_modules
+const path = require('path'); // core do Node.js
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // gera o html em dist automaticamente com base em um modelo na pasta src
 
 module.exports = {
   // informando ao Webpack onde estarão os arquivos-fontes
@@ -15,8 +15,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'src/index.html')
+      filename: 'index.html', // arquivo gerado em dist
+      template: path.join(__dirname, 'src/index.html') // template do arquivo em src
     })
   ],
   module: {
@@ -25,22 +25,38 @@ module.exports = {
         test: /.jsx?$/, // regex para informar ao webpack a extensão do arquivo
         exclude: /node_modules/,
         include: path.join(__dirname, 'src'), // diretório dos arquivos de origem
-        use: [{
-          loader: 'babel-loader', // se os passos acima forem atendidos, executa o babel...
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'] // ... com os seguintes presets
+        use: [
+          {
+            loader: 'babel-loader', // se os passos acima forem atendidos, executa o babel...
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'] // ... com os seguintes presets
+            }
           }
-        }]
+        ]
       },
       {
         test: /\.(jpe?g|ico|png|gif|svg)$/i,
         // loader: 'file-loader?name=img/[name].[ext]'    // código antigo
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'img/[name].[ext]'
+        use: [
+          {
+            loader: 'file-loader', // para que as imagens utilizadas no index.html do src sejam transferidas para o build
+            options: {
+              name: 'img/[name].[ext]'
+            }
           }
-        }]
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // css-loader e style-loader trabalham em conjunto, o 1º fazendo o require dos arquivos e o 2º aplicando0o na página
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
       }
     ]
   },
@@ -50,4 +66,4 @@ module.exports = {
     }
   },
   mode: 'development'
-}
+};
