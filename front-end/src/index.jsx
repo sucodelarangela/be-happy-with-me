78eb502cbdfd64814539aa8bb8741e370a6bb548
Angela Caldas <aes.caldas@gmail.com>
2022-05-23 19:09:41 -0300

    Creating App component

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
 import './img/favicon.ico'
 import './css/index.css'
 
 const root = ReactDOM.createRoot(document.querySelector('#main'))
 
 root.render(
-  <h1>Bem vindo ao React!</h1>
+  <App />
 )
