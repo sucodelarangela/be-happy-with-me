aa2f2849a0a19b66339584193a99525d9c46b149
Angela Caldas <aes.caldas@gmail.com>
2022-05-25 12:58:40 -0300

    Fix pure.css install (cdn)

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import './img/logo.png';
 // Componentes stateless, considerados mais simples e que só precisam exibir dados estáticos, podem ser implementados com uma declaração mais simples, de 'function'
 export default function Header() {
   return (
-    <div className="header">
+    <div className="header pure-menu pure-menu-horizontal pure-menu-fixed">
       <a href="/">
         <img src="img/logo.png" className="logo" />
       </a>
