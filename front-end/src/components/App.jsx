fd69c11eba77ca780322f654c62abb4e3e7d8b68
Angela Caldas <aes.caldas@gmail.com>
2022-05-25 19:51:54 -0300

    Creating NovoUsuario component

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
+import NovoUsuario from './NovoUsuario';
 
 // É uma boa prática criar componentes stateful por meio da sintaxe 'class'.
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div>
         <Header />
-        <h1>Component App!</h1>
-      </>
-    )
+        <NovoUsuario />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NovoUsuario extends React.Component {
+  render() {
+    <div className="center"></div>;
+  }
+}
+
+export default NovoUsuario;
