04a63b26cb1392a49774e456026b2ee6d94d0b88
Angela Caldas <aes.caldas@gmail.com>
2022-05-28 16:26:36 -0300

    Final version of Input component

--- front-end/src/components/Input/index.jsx
+++ front-end/src/components/Input/index.jsx
@@ -7,12 +7,16 @@ export default function Input(props) {
     backgroundColor: props.valorInvalido ? '#ffcdd2' : '#ffffff'
   };
 
+  // O método assign() copia todos os atributos de um ou mais objetos para outro objeto de destino. Aqui estamos copiando this.props para dentro de propriedades, apagando o atributo propriedades.valorInvalido e repassando o objeto integralmente à tag input
+  let propriedades = Object.assign({}, props);
+  delete propriedades.valorInvalido;
+
   return (
     <input
       //usando o spread operator, podemos passar todas as propriedades sem relacioná-las uma a uma
       type="text"
       style={estilo}
-      {...props}
+      {...propriedades}
       //id={props.id}
       //placeholder={props.placeholder}
       //maxLength={props.maxLength}
