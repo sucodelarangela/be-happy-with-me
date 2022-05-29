63bb56f27a67124ab1a7a6536ea6ae51510f3284
Angela Caldas <aes.caldas@gmail.com>
2022-05-29 16:38:57 -0300

    Adjusting NovoUsuario component

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -6,8 +6,21 @@ class NovoUsuario extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nomeInvalido: true
+      usuario: {
+        nome: ''
+      },
+      validacao: {
+        nomeInvalido: false
+      }
     };
+  }
+
+  atualizarNome(e) {
+    let usuario = this.state.usuario;
+    usuario.nome = e.target.value;
+    this.setState({
+      usuario: usuario
+    });
   }
 
   render() {
@@ -17,15 +30,16 @@ class NovoUsuario extends React.Component {
           <Label
             htmlFor="nome"
             texto="Quem é você?"
-            valorInvalido={this.state.nomeInvalido}
+            valorInvalido={this.state.validacao.nomeInvalido}
           />
           <Input
             id="nome"
             placeholder="Digite seu nome"
             maxLength="40"
             readOnly={false}
-            valorInvalido={this.state.nomeInvalido}
-            defaultValue="Angela"
+            valorInvalido={this.state.validacao.nomeInvalido}
+            defaultValue={this.state.usuario.nome}
+            onChange={this.atualizarNome.bind(this)}
           />
         </form>
       </div>
